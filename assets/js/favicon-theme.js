00178bda91b5b8585203a6274ebda90cba9065fb
Gavin Warner <gavin@dwarnerii.com>
2026-04-30 21:08:28 -0700

    Updating Icon For light and dark mode

--- assets/js/favicon-theme.js
+++ assets/js/favicon-theme.js
@@ -3,20 +3,20 @@ function setFaviconForTheme(theme) {
   const faviconShortcut = document.getElementById("favicon-shortcut");
   const favicon32 = document.getElementById("favicon-32");
   const favicon16 = document.getElementById("favicon-16");
-  const version = "?v=4";
+  const version = "?v=5";
 
   if (!faviconShortcut || !favicon32 || !favicon16) return;
 
   faviconShortcut.href = isDark
-    ? "/images/favicons/favicon-dark-32.png" + version
-    : "/images/favicons/favicon-light-32.png" + version;
+    ? "/images/favicons/favicon-light-32.png" + version
+    : "/images/favicons/favicon-dark-32.png" + version;
 
   favicon32.href = isDark
-    ? "/images/favicons/favicon-dark-32.png" + version
-    : "/images/favicons/favicon-light-32.png" + version;
+    ? "/images/favicons/favicon-light-32.png" + version
+    : "/images/favicons/favicon-dark-32.png" + version;
   favicon16.href = isDark
-    ? "/images/favicons/favicon-dark-16.png" + version
-    : "/images/favicons/favicon-light-16.png" + version;
+    ? "/images/favicons/favicon-light-16.png" + version
+    : "/images/favicons/favicon-dark-16.png" + version;
 }
 
 function getActiveTheme() {
